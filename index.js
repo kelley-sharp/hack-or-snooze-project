39e601c3c679cc99415fa4e6631bf69a1b2f1234
kelley <kelleylsharp@gmail.com>
2018-06-06 16:18:00 -0700

    login works and goes away

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function() {
     .then(function(data) {
       data.data.forEach(function(name) {
         list.append(`<li>${name.title} (${name.url})</li>`);
-        console.log(name.title);
+        // console.log(name.title);
       });
     })
     .catch(function(error) {
@@ -21,3 +21,24 @@ $('#signUpLink').on('click', function() {
   $('#loginForm').hide();
   $('#signUpForm').slideToggle();
 });
+
+$('#loginForm').submit(logIn);
+
+function logIn(e) {
+  e.preventDefault();
+  let data = {
+    data: {
+      username: $('#login_username').val(),
+      password: $('#login_password').val()
+    }
+  };
+  $.post('https://hack-or-snooze.herokuapp.com/auth/', data, 'json')
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      $('#loginForm').slideToggle();
+      $('#loginForm > form')[0].reset();
+      $('#loginLink').hide();
+      $('#signUpLink').hide();
+    })
+    .catch(err => console.log(err));
+}
